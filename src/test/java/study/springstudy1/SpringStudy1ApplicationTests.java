df0a4f5dc5fe0f6a61c852098fe8450ae491a974
kklee0930 <kklee0930@gmail.com>
2023-09-04 19:37:46 +0900

    230904

---
+++ src/test/java/study/springstudy1/SpringStudy1ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.springstudy1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudy1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
